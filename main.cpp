7a1c28e60a9034bb135e4cec927eb86469b8a594
Karim <karimchehab2018@gmail.com>
2023-04-18 02:11:32 +0200

    Added Register and Login Functions in main.cpp Tested it in main and it works in data.ini

--- main.cpp
+++ main.cpp
@@ -11,8 +11,31 @@ void TestAccountToConfig()
 	UserAccount acc(50, "muhab", "12345666");
 	cfg.UserAccounts[acc.ID()] = acc;
 
+	
+
 	// Save configuration on exit (not finished yet)
 	cfg.Save();
+}
+
+bool Register(string Username, string Password)
+{
+	if (cfg.AccountExists(Username))
+		return false;
+	else
+	{
+		int new_id = cfg.UserAccounts.size() + 1;
+		UserAccount new_acc(new_id, Username, Password);
+		cfg.UserAccounts[new_acc.ID()] = new_acc;
+		return true;
+	}
+}
+
+bool Login(string Username, string Password)
+{
+	if (cfg.AccountExists(Username, Password))
+		return true;
+	else
+		return false;
 }
 
 int main()
@@ -23,7 +46,21 @@ int main()
 	// Testing configuration saving
 	//TestAccountToConfig();
 
+	//Testing Register
+	/*if (Register("Karim", "123123"))
+		cout << "Yes\n";
+	else
+		cout << "No\n";*/
+
+	//Testing Login
+	/*if (Login("Karim", "123123"))
+		cout << "Yes\n";
+	else
+		cout << "No\n";*/
+
 	// Save configuration on exit (not finished yet)
 	cfg.Save();
 	return 0;
 }
+
+
