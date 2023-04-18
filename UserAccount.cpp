e6034d3be3c1f3298789bdb358ba53848aa82697
Mohamed Wael <fennekinflame30@gmail.com>
2023-04-18 05:20:30 +0200

    Adding UserAccount Functions for StartUp

--- UserAccount.cpp
+++ UserAccount.cpp
@@ -30,3 +30,39 @@ string UserAccount::Password(void) const
 {
 	return m_password;
 }
+bool UserAccount::AddContact(int User_ID) {
+
+}
+
+bool UserAccount::RemoveContact(int User_ID) {
+
+}
+void UserAccount::SendMessage(int User_ID, string content) {
+
+}
+bool UserAccount::PopMessage(int User_ID) {
+
+}
+void UserAccount::ViewContacts() { //ordered by most messages
+}
+void UserAccount::ViewMessages() {
+
+}
+void UserAccount::ViewMessages(int User_ID) {
+
+}
+bool UserAccount::PutFavorite(int User_ID, int Msg_Index) {
+
+}
+bool UserAccount::RemoveOldestFavorite(int User_ID) {
+
+}
+void UserAccount::ViewFavorites() {
+
+}
+int UserAccount::GetMessagesFromUser(UserAccount* user) {
+
+}
+UserAccount* UserAccount::GetContact(int User_ID) {
+
+}
--- UserAccount.h
+++ UserAccount.h
@@ -18,7 +18,18 @@ public:
 	UserAccount(int id, string username, string pw);
 
 	void Set(int id, string username, string pw);
-
+	bool AddContact(int User_ID);
+	bool RemoveContact(int User_ID);
+	void SendMessage(int User_ID, string content);
+	bool PopMessage(int User_ID);
+	void ViewContacts(); //ordered by most messages
+	void ViewMessages();
+	void ViewMessages(int User_ID);
+	bool PutFavorite(int User_ID, int Msg_Index);
+	bool RemoveOldestFavorite(int User_ID);
+	void ViewFavorites();
+	int GetMessagesFromUser(UserAccount* user);
+	UserAccount* GetContact(int User_ID);
 	int ID() const;
 	string Username() const;
 	string Password() const;
